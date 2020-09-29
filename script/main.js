eda8549beaee1ef38e2f3ca7aa7881789c19218c
Leskin Maksim <Quper2405@gmail.com>
2020-09-29 21:20:57 +0200

    ready translation

--- script/main.js
+++ script/main.js
@@ -1,22 +1,28 @@
-
+'use strict';
 
 // day1
 
 const btnBurger = document.querySelector('.btn-burger');
 const catalog = document.querySelector('.catalog');
-const overlay = document.querySelector('.overlay');
 const btnClose = document.querySelector('.btn-close');
 const subCatalog = document.querySelector('.subcatalog');
 const subcatalogHeader = document.querySelector('.subcatalog-header');
+const btnReturn = document.querySelector('.btn-return');
+
+const overlay = document.createElement('div');
+overlay.classList.add('overlay');
+document.body.insertAdjacentElement('beforeend', overlay);
+
 
 const openMenu = () => { 
     catalog.classList.add('open');
     overlay.classList.add('active');
 };
 
-const closeMenu = () => { 
+const closeMenu = () => {
+    closeSubMenu();
     catalog.classList.remove('open');
-    overlay.classList.remove('active');
+    overlay.classList.remove('active');   
 };
 
 const openSubMenu = (event) => {
@@ -28,11 +34,15 @@ const openSubMenu = (event) => {
     };
 };
 
+const closeSubMenu = () => {
+    subCatalog.classList.remove('subopen');
+}
 
 btnBurger.addEventListener('click', openMenu);
 btnClose.addEventListener('click', closeMenu);
 overlay.addEventListener('click', closeMenu);
 catalog.addEventListener('click', openSubMenu);
+btnReturn.addEventListener('click', closeSubMenu);
 
 
 
